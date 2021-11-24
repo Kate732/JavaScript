853c564df4250d62ca840d332f0025abb897ecaf
Kate_M <90863692+Kate732@users.noreply.github.com>
2021-11-24 10:09:41 +0200

    Update and rename 0-buggy.js to 1-fixed.js

---
+++ 1-fixed.js
@@ -0,0 +1,9 @@
+let first_num = 2;
+let secord_num = 3;
+let sum = firstNum + secondNum;
+alert(sum);
+
+const firstNum = 2;
+const secondNum = 3;
+const sum = firstNum + secondNum;
+console.log(sum);
